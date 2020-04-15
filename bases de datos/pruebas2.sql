38fa1e4c9552f80ebe9361056e50d4acc065a2b7
carlmrx <43156603+carlmrx@users.noreply.github.com>
2020-04-15 02:33:36 -0500

    MEJORAS

--- bases de datos/pruebas2.sql
+++ bases de datos/pruebas2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-04-2020 a las 00:45:48
+-- Tiempo de generación: 15-04-2020 a las 09:32:36
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.3
 
@@ -31,17 +31,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
+  `correo` varchar(60) NOT NULL,
   `apellido` varchar(50) DEFAULT NULL,
   `usuario` varchar(50) DEFAULT NULL,
   `password` tinytext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `usuario`, `password`) VALUES
-(1, 'carlos', 'serrano cruz', 'carlos', '8cb2237d0679ca88db6464eac60da96345513964');
 
 --
 -- Índices para tablas volcadas
@@ -61,7 +55,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
